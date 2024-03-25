7c012c544181be9e4ab8886c614f9bafab678edf
Manuel Roshardt <manuelroshardt@gmail.com>
2024-03-25 21:52:32 +0000

    Added three windows to display the estimated robot position using the /odom topic to get X, Y, and Z

--- include/robot_gui/robot_gui.h
+++ include/robot_gui/robot_gui.h
@@ -2,9 +2,9 @@
 
 #define CVUI_IMPLEMENTATION
 #include "robot_gui/cvui.h"
-#include "std_msgs/Float64.h"
 #include <robotinfo_msgs/RobotInfo10Fields.h>
 #include <geometry_msgs/Twist.h>
+#include "nav_msgs/Odometry.h"
 #include <opencv2/opencv.hpp>
 #include <ros/ros.h>
 
@@ -14,14 +14,18 @@ class RobotGUI {
         void run();
 
     private:
-        ros::Subscriber sub_;
+        ros::Subscriber info_sub_;
+        ros::Subscriber odom_sub_;
         ros::Publisher twist_pub_;
         robotinfo_msgs::RobotInfo10Fields robot_data;
-        std::string topic_name;
+        std::string info_topic_name;
         std::string twist_topic_name;
         geometry_msgs::Twist twist_msg;
         float linear_velocity_step = 0.1;
         float angular_velocity_step = 0.1;
         void infoCallback(const robotinfo_msgs::RobotInfo10Fields::ConstPtr &msg);
+        nav_msgs::Odometry odom_data;
+        std::string odom_topic_name;
+        void odomCallback(const nav_msgs::Odometry::ConstPtr &msg);
         const std::string WINDOW_NAME = "ROBOT GUI";
 };
--- src/robot_gui.cpp
+++ src/robot_gui.cpp
@@ -2,31 +2,39 @@
 
 RobotGUI::RobotGUI() {
   ros::NodeHandle nh;
-  topic_name = "robot_info";
-  sub_ = nh.subscribe<robotinfo_msgs::RobotInfo10Fields>(topic_name, 2,
-                                         &RobotGUI::infoCallback, this);
+  info_topic_name = "robot_info";
+  info_sub_ = nh.subscribe<robotinfo_msgs::RobotInfo10Fields>(
+      info_topic_name, 2, &RobotGUI::infoCallback, this);
   twist_topic_name = "cmd_vel";
   twist_pub_ = nh.advertise<geometry_msgs::Twist>(twist_topic_name, 10);
+
+  odom_topic_name = "odom";
+  odom_sub_ = nh.subscribe<nav_msgs::Odometry>(odom_topic_name, 2,
+                                               &RobotGUI::odomCallback, this);
 }
 
-void RobotGUI::infoCallback(const robotinfo_msgs::RobotInfo10Fields::ConstPtr &msg)
-{
-    robot_data = *msg;
+void RobotGUI::infoCallback(
+    const robotinfo_msgs::RobotInfo10Fields::ConstPtr &msg) {
+  robot_data = *msg;
 
-    ROS_DEBUG("Robot data received:");
-    ROS_DEBUG("  data_field_01: %s", msg->data_field_01.c_str());
-    ROS_DEBUG("  data_field_02: %s", msg->data_field_02.c_str());
-    ROS_DEBUG("  data_field_03: %s", msg->data_field_03.c_str());
-    ROS_DEBUG("  data_field_04: %s", msg->data_field_04.c_str());
-    ROS_DEBUG("  data_field_05: %s", msg->data_field_05.c_str());
-    ROS_DEBUG("  data_field_06: %s", msg->data_field_06.c_str());
-    ROS_DEBUG("  data_field_07: %s", msg->data_field_07.c_str());
-    ROS_DEBUG("  data_field_08: %s", msg->data_field_08.c_str());
-    ROS_DEBUG("  data_field_09: %s", msg->data_field_09.c_str());
-    ROS_DEBUG("  data_field_10: %s", msg->data_field_10.c_str());
+  ROS_DEBUG("Robot data received:");
+  ROS_DEBUG("  data_field_01: %s", msg->data_field_01.c_str());
+  ROS_DEBUG("  data_field_02: %s", msg->data_field_02.c_str());
+  ROS_DEBUG("  data_field_03: %s", msg->data_field_03.c_str());
+  ROS_DEBUG("  data_field_04: %s", msg->data_field_04.c_str());
+  ROS_DEBUG("  data_field_05: %s", msg->data_field_05.c_str());
+  ROS_DEBUG("  data_field_06: %s", msg->data_field_06.c_str());
+  ROS_DEBUG("  data_field_07: %s", msg->data_field_07.c_str());
+  ROS_DEBUG("  data_field_08: %s", msg->data_field_08.c_str());
+  ROS_DEBUG("  data_field_09: %s", msg->data_field_09.c_str());
+  ROS_DEBUG("  data_field_10: %s", msg->data_field_10.c_str());
 }
 
-
+void RobotGUI::odomCallback(const nav_msgs::Odometry::ConstPtr &msg) {
+  odom_data = *msg;
+  ROS_DEBUG("Position x,y,z: [%0.2f, %0.2f, %0.2f]", msg->pose.pose.position.x,
+            msg->pose.pose.position.y, msg->pose.pose.position.z);
+}
 
 void RobotGUI::run() {
   cv::Mat frame = cv::Mat(600, 450, CV_8UC3);
@@ -39,50 +47,74 @@ void RobotGUI::run() {
 
     cvui::window(frame, 10, 20, 430, 190, "Info");
 
-    cvui::printf(frame, 15, 45, 0.4, 0xffffff, robot_data.data_field_01.c_str());
-    cvui::printf(frame, 15, 65, 0.4, 0xffffff, robot_data.data_field_02.c_str());
-    cvui::printf(frame, 15, 85, 0.4, 0xffffff, robot_data.data_field_03.c_str());
-    cvui::printf(frame, 15, 105, 0.4, 0xffffff, robot_data.data_field_04.c_str());
-    cvui::printf(frame, 15, 125, 0.4, 0xffffff, robot_data.data_field_05.c_str());
-    cvui::printf(frame, 15, 145, 0.4, 0xffffff, robot_data.data_field_06.c_str());
-    cvui::printf(frame, 15, 165, 0.4, 0xffffff, robot_data.data_field_07.c_str());
-    cvui::printf(frame, 15, 185, 0.4, 0xffffff, robot_data.data_field_08.c_str());
+    cvui::printf(frame, 15, 45, 0.4, 0xffffff,
+                 robot_data.data_field_01.c_str());
+    cvui::printf(frame, 15, 65, 0.4, 0xffffff,
+                 robot_data.data_field_02.c_str());
+    cvui::printf(frame, 15, 85, 0.4, 0xffffff,
+                 robot_data.data_field_03.c_str());
+    cvui::printf(frame, 15, 105, 0.4, 0xffffff,
+                 robot_data.data_field_04.c_str());
+    cvui::printf(frame, 15, 125, 0.4, 0xffffff,
+                 robot_data.data_field_05.c_str());
+    cvui::printf(frame, 15, 145, 0.4, 0xffffff,
+                 robot_data.data_field_06.c_str());
+    cvui::printf(frame, 15, 165, 0.4, 0xffffff,
+                 robot_data.data_field_07.c_str());
+    cvui::printf(frame, 15, 185, 0.4, 0xffffff,
+                 robot_data.data_field_08.c_str());
 
     int button_y = 220;
     if (cvui::button(frame, 160, button_y, 130, 60, " Forward ")) {
-        twist_msg.linear.x = twist_msg.linear.x + linear_velocity_step;
-        twist_pub_.publish(twist_msg);
+      twist_msg.linear.x = twist_msg.linear.x + linear_velocity_step;
+      twist_pub_.publish(twist_msg);
     }
 
-    button_y += 65; 
+    button_y += 65;
     if (cvui::button(frame, 25, button_y, 130, 60, " Left ")) {
-        twist_msg.angular.z = twist_msg.angular.z + angular_velocity_step;
-        twist_pub_.publish(twist_msg);
+      twist_msg.angular.z = twist_msg.angular.z + angular_velocity_step;
+      twist_pub_.publish(twist_msg);
     }
 
     if (cvui::button(frame, 160, button_y, 130, 60, "   Stop  ")) {
-        twist_msg.linear.x = 0.0;
-        twist_msg.angular.z = 0.0;
-        twist_pub_.publish(twist_msg);
+      twist_msg.linear.x = 0.0;
+      twist_msg.angular.z = 0.0;
+      twist_pub_.publish(twist_msg);
     }
 
     if (cvui::button(frame, 295, button_y, 130, 60, " Right ")) {
-        twist_msg.angular.z = twist_msg.angular.z - angular_velocity_step;
-        twist_pub_.publish(twist_msg);
+      twist_msg.angular.z = twist_msg.angular.z - angular_velocity_step;
+      twist_pub_.publish(twist_msg);
     }
 
-    button_y += 65; 
+    button_y += 65;
     if (cvui::button(frame, 160, button_y, 130, 60, "Backward")) {
-        twist_msg.linear.x = twist_msg.linear.x - linear_velocity_step;
-        twist_pub_.publish(twist_msg);
+      twist_msg.linear.x = twist_msg.linear.x - linear_velocity_step;
+      twist_pub_.publish(twist_msg);
     }
 
     int velocity_y = 420;
     cvui::window(frame, 10, velocity_y, 210, 40, "Linear velocity:");
-    cvui::printf(frame, 35, velocity_y + 25, 0.4, 0xff0000, "%.02f m/sec", twist_msg.linear.x);
+    cvui::printf(frame, 35, velocity_y + 25, 0.4, 0xff0000, "%.02f m/sec",
+                 twist_msg.linear.x);
 
     cvui::window(frame, 230, velocity_y, 210, 40, "Angular velocity:");
-    cvui::printf(frame, 255, velocity_y + 25, 0.4, 0xff0000, "%.02f rad/sec", twist_msg.angular.z);
+    cvui::printf(frame, 255, velocity_y + 25, 0.4, 0xff0000, "%.02f rad/sec",
+                 twist_msg.angular.z);
+
+    cvui::text(frame, 25, 480, "Estimated robot position based off odometry");
+
+    cvui::window(frame, 25, 495, 130, 90, "X");
+    cvui::printf(frame, 25, 520, 0.4, 0xffffff, "%.02f",
+                 odom_data.pose.pose.position.x);
+
+    cvui::window(frame, 160, 495, 130, 90, "Y");
+    cvui::printf(frame, 160, 520, 0.4, 0xffffff, "%.02f",
+                 odom_data.pose.pose.position.y);
+
+    cvui::window(frame, 295, 495, 130, 90, "Z");
+    cvui::printf(frame, 295, 520, 0.4, 0xffffff, "%.02f",
+                 odom_data.pose.pose.position.z);
 
     cvui::update();
 
